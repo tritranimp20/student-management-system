066b18fcd2f77b6b9f937b5de5f978a7e8d865e5
Tri Tran <tri.tran.imp20@hcmut.edu.vn>
2021-12-11 07:55:12 +0700

    Lab 2

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# student-management-system
-Student Management System
+# Mobile Systems Security
+**LAB 2 _ Prepared by Dr Han Duy Phan**
+
+
+### Homework: 
+You start building a student management system and Android application for your school. 
+In this lab 2, you start building the simple application with three activities that look roughly like this. For lab 2 you just need to build Activity 1 and 2.
+
+#### Activity 1 (Home Screen)
+Label Application Name: Student Management System 
+Label App Developer: Your Name
+Button Add New Student
+	Start Activity 2
+Button View All Students
+	Start Activity 3
+
+#### Activity 2 (Add New Student)
+Label Student Name EditText
+Label Student Score EditText
+Button ADD 
+	just show a toast that say student is added for now
+
+#### Activity 3 (Display The List Of All Students)
+... More information will be added for lab 3, just leave this activity empty for now
+
+
+### REFERENCES:
+https://developer.android.com/training/basics/firstapp/building-ui
+https://developer.android.com/training/basics/firstapp/starting-activity
+https://developer.android.com/guide/topics/ui/notifiers/toasts
+
+ 
---
+++ app/src/main/java/tritran/imp20/student_managementsystem/AddStudentFragment.java
@@ -0,0 +1,49 @@
+package tritran.imp20.student_managementsystem;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+public class AddStudentFragment extends Fragment {
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_student, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button_all).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(AddStudentFragment.this)
+                        .navigate(R.id.action_AddStudentFragment_to_MainFragment);
+            }
+        });
+
+        view.findViewById(R.id.btn_add).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = AddStudentFragment.this.getContext();
+                CharSequence text = AddStudentFragment.this.getResources().getString(R.string.add_new_student_successfully);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+    }
+
+}
